a79d2154394c64aa264e65baf74836d22e0bc4f2
Sahil Amirov <69482519+SahilEmirov@users.noreply.github.com>
2023-04-28 22:27:30 +0300

    some1

---
+++ ProjeTodo/Core/CheckString.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjeTodo.Core
+{
+    public class CheckString
+    {
+        public static bool Check(string value)
+        {
+            bool result = true;
+            if (value.Contains("script")) result = false;
+            if (value.Contains("SCRIPT")) result = false;
+            if (value.Contains("alert")) result = false;
+            if (value.Contains("$")) result = false;
+            if (value.Contains("~")) result = false;
+            if (value.Contains("[")) result = false;
+            if (value.Contains("^")) result = false;
+            if (value.Contains("<")) result = false;
+            if (value.Contains(">")) result = false;
+            if (value.Contains("--")) result = false;
+            if (value.Contains("`")) result = false;
+            if (value.Contains("\\")) result = false;
+            if (value.Contains("#")) result = false;
+            if (value.Contains("/*")) result = false;
+            if (value.Contains("*/")) result = false;
+            if (value.Contains("SELECT")) result = false;
+            return result;
+        }
+    }
+}
---
+++ ProjeTodo/DataAccess/ConnectionString.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApp.Dal
+{
+    public class ConnectionString
+    {
+        public static string ConnectionValue = "Server=104.247.162.242\\MSSQLSERVER2017;Database=akadem58_xsence;User Id=akadem58_xsence;Password=Jp#6866bd;";
+    }
+}
